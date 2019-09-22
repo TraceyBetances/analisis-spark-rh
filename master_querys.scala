248cd7786471714e9cfa69781a86bc186327dfde
TraceyBetances <54511221+TraceyBetances@users.noreply.github.com>
2019-09-22 21:06:21 +0200

    object controlador de querys

---
+++ master_querys.scala
@@ -0,0 +1,68 @@
+package peticiones
+
+import org.apache.spark.sql.SparkSession
+
+object master_querys {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder.getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+
+    val control_querys = new RRHH_requests(args(0), spark)
+
+    val flagControl_DF_query1 = true
+    val flagControl_DF_query2 = true
+    val flagControl_DF_query3 = true
+    val flagControl_DF_query4 = false
+    val flagControl_DF_query5 = false
+    val flagControl_DF_query6 = false
+    val flagControl_DF_query7 = false
+    val flagControl_DF_query8 = false
+
+    if (flagControl_DF_query1) {
+      //Control sae_dwc
+      control_querys.DF_query1()
+    }
+
+    if (flagControl_DF_query2) {
+      //Control sae_dwc
+      control_querys.DF_query2()
+    }
+
+    if (flagControl_DF_query3) {
+      //Control sae_dwc
+      control_querys.DF_query3()
+    }
+
+    if (flagControl_DF_query4) {
+      //Control sae_dwc
+      control_querys.DF_query4()
+    }
+
+    if (flagControl_DF_query5) {
+      //Control sae_dwc
+      control_querys.DF_query5()
+    }
+
+    if (flagControl_DF_query6) {
+      //Control sae_dwc
+      control_querys.DF_query6()
+    }
+
+    if (flagControl_DF_query7) {
+      //Control sae_dwc
+      control_querys.DF_query2()
+    }
+
+    if (flagControl_DF_query7) {
+      //Control sae_dwc
+      control_querys.DF_query2()
+    }
+
+    if (flagControl_DF_query8) {
+      //Control sae_dwc
+      control_querys.DF_query8()
+    }
+  }
+}
